0c7d19fe13b197eb739f97d90b7ab4bd8a07c376
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-09-24 14:13:32 +0700

    penambahan localization untuk en

---
+++ config/lang/en/label.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'users'      => 'Users',
+    'users_name' => 'Name',
+    'users_username' => 'Username',
+    'users_password' => 'Password',
+
+    'roles'      => 'Roles',
+    'roles_name' => 'Name',
+
+    'user_roles' => 'User Roles',
+    'user_roles_user' => 'User',
+    'user_roles_role' => 'Role',
+];
---
+++ config/lang/en/menu.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'module_name' => 'Default',
+    'users and roles' => 'Users and Roles',
+    'users' => 'Users',
+    'roles' => 'Roles'
+];
